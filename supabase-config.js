d1b26044b38a7c5e63542914be2b7e34b697dcb7
TopQpro <quinten.swalus@gmail.com>
2026-09-05 11:45:34 +0200

    Add files via upload
    Corrected the config-file

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,5 @@
-const SUPABASE_URL = https://topqpro.github.io/survive-the-bots/
-const SUPABASE_KEY = sb_publishable_k7sEatMNh2lPj7WvL1CnyA_N0-4RGpL
+const SUPABASE_URL =
+    "https://neodeghkonbotgdsivwx.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_k7sEatMNh2lPj7WvL1CnyA_N0-4RGpL";
